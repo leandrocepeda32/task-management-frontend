0dfe7683b48e45622769c857b327f5a6d51ef4bd
leandrocepeda32 <leandrocepeda32@users.noreply.github.com>
2021-08-04 04:04:26 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.ae996255c1ccefe561620fe060ec26c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d755e3733242fb1535e5d65dea3e9b",
+    "url": "https://leandrocepeda32.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "bcc59173a2893f14842f",
+    "url": "https://leandrocepeda32.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "33c0732eee9ac0b8f866",
+    "url": "https://leandrocepeda32.github.io/task-management-frontend/static/js/2.6a15f103.chunk.js"
+  },
+  {
+    "revision": "bcc59173a2893f14842f",
+    "url": "https://leandrocepeda32.github.io/task-management-frontend/static/js/main.2affd2f8.chunk.js"
+  },
+  {
+    "revision": "e158ce4272b4b04663bc",
+    "url": "https://leandrocepeda32.github.io/task-management-frontend/static/js/runtime~main.3c9e38aa.js"
+  }
+]);
